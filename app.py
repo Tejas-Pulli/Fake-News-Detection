f069aaee48c4c0b4c2baa7b545b11dc599e1d3ab
Tejas-Pulli <115936478+Tejas-Pulli@users.noreply.github.com>
2023-12-04 00:41:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import pickle
+import streamlit as st
+# import numpy as np
+
+# loading the saved models
+with open('D:/Work/DL/Fack_News/vectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
+
+with open('D:/Work/DL/Fack_News/fake_news.sav', 'rb') as file:
+    news_models = pickle.load(file)
+
+
+#page title
+
+# getting the input data from the user
+Title = st.text_input("Enter Title of News")
+Author = st.text_input("Enter Author's Name")
+Text = st.text_input("Enter News Text")
+
+input_data = [Title+' '+Author+' '+Text]
+# input_data_reshaped = input_data.reshape(1,-1)
+
+input_transformed = vectorizer.transform(input_data)
+
+
+#code for Prediction
+news_pred = ''
+
+#creating a button for prediction
+if st.button('Check News'):
+    news_prediction = news_models.predict(input_transformed)
+    
+    if(news_prediction[0]==1):
+        news_pred='Yes'
+    else:
+        news_pred='No'
+st.success(news_pred)
